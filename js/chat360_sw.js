2ba9bb53582c895dbda3a8419f5842cefb55df25
Atul Patil <atulp441@gmail.com>
2025-03-22 16:03:00 +0530

    Update chat360_sw.js

--- js/chat360_sw.js
+++ js/chat360_sw.js
@@ -1,1 +1,6 @@
 
+var chat360Config = {
+  channelId: "16e651e5-f781-4bd3-ba5f-f600a19f5311",
+  key: "BE3FXol_7WMeMVQpYUEf1oPd6c3miXVsdHfaRmt3UslTLtFQKZV8GWOkpo9b4WLvt2pCs7Cc30CL5XEwIjb0WUk"
+};
+importScripts("https://staging.chat360.io/push/service_worker/sw.js");
