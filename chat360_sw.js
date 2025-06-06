42948c6c63edc08e0b62fae198d1e64361c8a546
Atul Patil <atulp441@gmail.com>
2025-06-06 17:09:45 +0530

    added chat360 code

---
+++ chat360_sw.js
@@ -0,0 +1,5 @@
+var chat360Config = {
+  channelId: "7a585bb9-0e18-498d-99f2-5c98cea2a53e",
+  key: "BAvxRQ-ngJkuhjzMUsWBVpC5zxTN8L2bRkOC5Ln7HzyU7cVulax-WaQ07vSZl1_MLRQuO5Pl769nUchM3fGKTWs"
+};
+importScripts("https://staging.chat360.io/push/service_worker/sw.js");
