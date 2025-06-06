5ed60717130ab65ef92bbfa58faa28c6fe8cbea0
Atul Patil <atulp441@gmail.com>
2025-06-06 12:09:48 +0530

    start form start

--- js/chat360_sw.js
+++
@@ -1,5 +0,0 @@
-var chat360Config = {
-  channelId: "16e651e5-f781-4bd3-ba5f-f600a19f5311",
-  key: "BE3FXol_7WMeMVQpYUEf1oPd6c3miXVsdHfaRmt3UslTLtFQKZV8GWOkpo9b4WLvt2pCs7Cc30CL5XEwIjb0WUk"
-};
-importScripts("https://staging.chat360.io/push/service_worker/sw.js");
